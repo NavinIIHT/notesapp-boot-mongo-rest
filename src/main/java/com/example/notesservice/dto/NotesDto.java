a3d5ad106a7886687696359fd637b0fddd63986b
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2020-09-08 12:38:25 +0530

    Add files via upload

---
+++ src/main/java/com/example/notesservice/dto/NotesDto.java
@@ -0,0 +1,18 @@
+package com.example.notesservice.dto;
+
+import lombok.Data;
+
+@Data
+public class NotesDto {
+	
+	private String id;
+	
+	private String title;
+	
+	private String author;
+	
+	private String description;
+	
+	private String status;
+	
+}
